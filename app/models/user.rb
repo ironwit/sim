99fa9179979e122e71a8469678940948d2f64f7d
ironwit@gmail.com <ironwit@gmail.com>
2022-06-16 11:30:41 +0300

    change route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :strayed, :class_name => 'Event', :foreign_key => 'strayed_id'
-  has_many :rescuer, :class_name => 'Event', :foreign_key => 'rescuer_id'
+  has_many :strays, :class_name => 'Event', :foreign_key => 'strayed_id'
+  has_many :rescuers, :class_name => 'Event', :foreign_key => 'rescuer_id'
 end
