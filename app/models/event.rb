35cfa4a0b7c41bce66683a453e50b510b1f76df6
ironwit@gmail.com <ironwit@gmail.com>
2022-06-16 09:29:16 +0300

    added relations between events and users

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  belongs_to :strayed, :class_name => 'User', :foreign_key => 'strayed_id', :validate => true
+  belongs_to :rescuer, :class_name => 'User', :foreign_key => 'rescuer_id', :validate => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :strayed, :class_name => 'Event', :foreign_key => 'strayed_id'
+  has_many :rescuer, :class_name => 'Event', :foreign_key => 'rescuer_id'
 end
